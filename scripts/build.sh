345c62b773de1735c2e91a7c5049617e668301ab
mieubrisse <mieubrisse@gmail.com>
2021-07-19 14:38:31 -0500

    Update build.sh to build the right image

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,20 @@
-set -euo pipefail
-script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
+#!/usr/bin/env bash
+# ^^^^^^^^^^^^^^^^^ this is the most platform-agnostic way to guarantee this script runs with Bash
+# 2021-07-08 WATERMARK, DO NOT REMOVE - This script was generated from the Kurtosis Bash script template
+
+set -euo pipefail   # Bash "strict mode"
+script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
 
-docker build -t "mieubrisse/datastore-army-lambda" --progress=plain -f "${root_dirpath}/Dockerfile" "${root_dirpath}"
+
+# ==================================================================================================
+#                                             Constants
+# ==================================================================================================
+KURTOSIS_DOCKERHUB_ORG="kurtosistech"
+IMAGE_NAME="datastore-army-lambda"
+
+
+# ==================================================================================================
+#                                             Main Logic
+# ==================================================================================================
+docker build -t "${KURTOSIS_DOCKERHUB_ORG}/${IMAGE_NAME}" --progress=plain -f "${root_dirpath}/Dockerfile" "${root_dirpath}"
